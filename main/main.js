7586d3c9e88051fb20e1796f2626306fd70faedc
xiongtai <xiongtaigo@live.com>
2019-05-29 13:26:45 +0800

    coding to pass all tests

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+const numbers = [
+    ['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.'],
+    ['|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|'],
+    ['|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|'],
+  ]
+
+module.exports = function main(number) {
+    var lcdNumber = [[], [], []]
+    for (var num of number) {
+        lcdNumber[0].push(numbers[0][num])
+        lcdNumber[1].push(numbers[1][num])
+        lcdNumber[2].push(numbers[2][num])
+    }
+
+    return lcdNumber.map(row => row.join(' ') + '\n').join('');
 };
